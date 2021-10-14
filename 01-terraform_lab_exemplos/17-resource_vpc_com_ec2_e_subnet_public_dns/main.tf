5327c45de5ba79ea98afcf52454d4fec20e20a66
Renato Dufloth <renatodufloth@hotmail.com>
2021-10-14 14:31:59 +0000

    ajustando informacoes

--- 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/main.tf
+++ 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/main.tf
@@ -3,18 +3,25 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami                     = data.aws_ami.ubuntu.id
-  instance_type           = "t3.micro"
-  key_name                = "Itau_treinamento" # key chave publica cadastrada na AWS 
-  subnet_id               =  aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
-  private_ip              = "172.17.0.100"
-  vpc_security_group_ids  = [
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+  key_name      = "key-dufloth-devout"    # key chave publica cadastrada na AWS 
+  subnet_id     = aws_subnet.my_subnet.id # vincula a subnet direto e gera o IP automático
+  private_ip    = "172.17.0.100"
+  vpc_security_group_ids = [
     "${aws_security_group.allow_ssh_terraform.id}",
   ]
 
   tags = {
-    Name = "Maquina para testar VPC do terraform"
+    Name = "dufloth-Maquina para testar VPC do terraform"
   }
+
+  root_block_device {
+    encrypted = true
+    #kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
+    volume_size = 20
+  }
+
 }
 
 resource "aws_eip" "example" {
@@ -31,6 +38,7 @@ resource "aws_eip_association" "eip_assoc" {
 output "aws_instance_e_ssh" {
   value = [
     aws_instance.web.public_ip,
-    "ssh -i id_rsa_itau_treinamento ubuntu@${aws_instance.web.public_dns}"
+    "ssh -i ~/.ssh/id_rsa  ubuntu@${aws_instance.web.public_dns}"
+    #"ssh -i id_rsa_itau_treinamento ubuntu@${aws_instance.web.public_dns}"
   ]
 }
--- 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/vpc.tf
+++ 01-terraform_lab_exemplos/17-resource_vpc_com_ec2_e_subnet_public_dns/vpc.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.17.0.0/16"
+  cidr_block           = "172.17.0.0/16"
   enable_dns_hostnames = true
 
   tags = {
-    Name = "TerraformVPCPublicSubnet"
+    Name = "dufloth-TerraformVPCPublicSubnet"
   }
 }
 
@@ -13,7 +13,7 @@ resource "aws_subnet" "my_subnet" {
   availability_zone = "us-east-1a"
 
   tags = {
-    Name = "tf-lab-danilo-subnet"
+    Name = "dufloth-tf-lab-danilo-subnet"
   }
 }
 
@@ -21,7 +21,7 @@ resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.my_vpc.id
 
   tags = {
-    Name = "aws_internet_gateway_terraform"
+    Name = "dufloth-aws_internet_gateway_terraform"
   }
 }
 
@@ -29,25 +29,25 @@ resource "aws_route_table" "rt_terraform" {
   vpc_id = aws_vpc.my_vpc.id
 
   route = [
-      {
-        carrier_gateway_id         = ""
-        cidr_block                 = "0.0.0.0/0"
-        destination_prefix_list_id = ""
-        egress_only_gateway_id     = ""
-        gateway_id                 = aws_internet_gateway.gw.id
-        instance_id                = ""
-        ipv6_cidr_block            = ""
-        local_gateway_id           = ""
-        nat_gateway_id             = ""
-        network_interface_id       = ""
-        transit_gateway_id         = ""
-        vpc_endpoint_id            = ""
-        vpc_peering_connection_id  = ""
-      }
+    {
+      carrier_gateway_id         = ""
+      cidr_block                 = "0.0.0.0/0"
+      destination_prefix_list_id = ""
+      egress_only_gateway_id     = ""
+      gateway_id                 = aws_internet_gateway.gw.id
+      instance_id                = ""
+      ipv6_cidr_block            = ""
+      local_gateway_id           = ""
+      nat_gateway_id             = ""
+      network_interface_id       = ""
+      transit_gateway_id         = ""
+      vpc_endpoint_id            = ""
+      vpc_peering_connection_id  = ""
+    }
   ]
 
   tags = {
-    Name = "route_table_terraform"
+    Name = "dufloth-route_table_terraform"
   }
 }
 
@@ -62,6 +62,6 @@ resource "aws_route_table_association" "a" {
 #   # security_groups = ["${aws_security_group.allow_ssh1.id}"]
 
 #   tags = {
-#     Name = "primary_network_interface my_subnet"
+#     Name = "dufloth-primary_network_interface my_subnet"
 #   }
 # }
