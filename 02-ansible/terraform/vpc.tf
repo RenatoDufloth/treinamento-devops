c137025cea914dbd6c5ef542f9d5e27f4ec01cf1
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-15 15:10:00 -0300

    Update vpc.tf

--- 02-ansible/terraform/vpc.tf
+++ 02-ansible/terraform/vpc.tf
@@ -9,19 +9,6 @@ resource "aws_security_group" "allow_ssh" {
 
  ingress = [
     {
-      description      = "80 from VPC"
-      from_port        = 80
-      to_port          = 80
-      protocol         = "tcp"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-      prefix_list_ids  = null
-      security_groups : null
-      self : null
-    }
-   
-   ingress = [
-    {
       description      = "ssh from VPC"
       from_port        = 22
       to_port          = 22
@@ -32,9 +19,7 @@ resource "aws_security_group" "allow_ssh" {
       security_groups : null
       self : null
     }
-
-
-  ]
+   
 
 
   egress = [
@@ -56,5 +41,44 @@ resource "aws_security_group" "allow_ssh" {
     Name = "allow_ssh"
   }
 }
+resource "aws_security_group" "allow_80" {
+  name        = "allow_80"
+  description = "Allow 80 inbound traffic"
+  vpc_id      = "vpc-0404e2502328d5e45"
 
+
+ ingress = [
+    {
+      description      = "80 from VPC"
+      from_port        = 80
+      to_port          = 80
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups : null
+      self : null
+    }
+   
+
+
+  egress = [
+    {
+      description      = "80 from VPC"
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+      ipv6_cidr_blocks = ["::/0"]
+      prefix_list_ids  = null
+      security_groups : null
+      self : null
+    }
+  ]
+
+
+  tags = {
+    Name = "allow_80"
+  }
+}
 
