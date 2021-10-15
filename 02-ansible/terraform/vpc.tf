a9424685adb191ce85850e7ba2cb0764bb6865b2
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-15 14:41:15 -0300

    Update vpc.tf

--- 02-ansible/terraform/vpc.tf
+++ 02-ansible/terraform/vpc.tf
@@ -10,8 +10,8 @@ resource "aws_security_group" "allow_ssh" {
   ingress = [
     {
       description      = "ssh from VPC"
-      from_port        = 22
-      to_port          = 22
+      from_port        = 80
+      to_port          = 80
       protocol         = "tcp"
       cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
