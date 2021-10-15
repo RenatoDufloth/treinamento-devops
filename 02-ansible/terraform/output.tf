37ebc38365ebf01767176d28e95bdf8a6c649e68
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-15 16:29:45 -0300

    Update output.tf

--- 02-ansible/terraform/output.tf
+++ 02-ansible/terraform/output.tf
@@ -1,7 +1,7 @@
 output "aws_instance_e_ssh" {
   value = [
     # for key, item in aws_instance.web :
-    "ec2  ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.web.public_dns} "
+    "ec2  ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.web.public_dns} ",
     "ec2  ssh -i ~/.ssh/id_rsa ubuntu@${aws_instance.web2.public_dns} "
 
   ]
