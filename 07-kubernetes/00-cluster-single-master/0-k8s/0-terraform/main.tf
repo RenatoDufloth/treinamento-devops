7d899b157c9102ce10115f5689963e17d1ab5446
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-19 10:26:13 -0300

    Update main.tf

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group" "acessos_master" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = [""0.0.0.0/0""]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids = null,
       security_groups: null,
