560cd228d0532e77cdc987a08c79b5388f8df1cd
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-19 09:56:58 -0300

    Update main.tf

--- 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
+++ 07-kubernetes/00-cluster-single-master/0-k8s/0-terraform/main.tf
@@ -22,7 +22,6 @@ resource "aws_instance" "maquina_master" {
   key_name      = "key-dufloth-devout" # key chave publica cadastrada na AWS
   subnet_id                   = "subnet-05cdfe4fe6a3d1c13"
   associate_public_ip_address = true
-  vpc_security_group_ids      = ["${aws_security_group.allow_ssh.id}", "${aws_security_group.allow_80.id}"]
   root_block_device {
     encrypted = true
     #kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
@@ -43,7 +42,6 @@ resource "aws_instance" "workers" {
   key_name      = "key-dufloth-devout" # key chave publica cadastrada na AWS
   subnet_id                   = "subnet-05cdfe4fe6a3d1c13"
   associate_public_ip_address = true
-  vpc_security_group_ids      = ["${aws_security_group.allow_ssh.id}", "${aws_security_group.allow_80.id}"]
   root_block_device {
     encrypted = true
     #kms_key_id  = "arn:aws:kms:us-east-1:534566538491:key/90847cc8-47e8-4a75-8a69-2dae39f0cc0d"
