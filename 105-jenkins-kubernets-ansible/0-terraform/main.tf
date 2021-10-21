4f39772f8a36984aaf2ef47721d8262c803b409c
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-21 17:40:17 -0300

    Update main.tf

--- 105-jenkins-kubernets-ansible/0-terraform/main.tf
+++ 105-jenkins-kubernets-ansible/0-terraform/main.tf
@@ -6,12 +6,42 @@ data "http" "myip" {
   url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
 }
 
+data "http" "myip" {
+  url = "http://ipv4.icanhazip.com" # outra opção "https://ifconfig.me"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+
+  owners = ["099720109477"] # Canonical
+}
+
+
 resource "aws_instance" "jenkins" {
-  ami           = "ami-09e67e426f25ce0d7"
+  ami           = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.large"
-  key_name      = "treinamento-turma1_itau"
+  key_name      = "key-dufloth-devout"
+  subnet_id                   = "subnet-05cdfe4fe6a3d1c13"
+  associate_public_ip_address = true
+  root_block_device {
+    encrypted = true
+    volume_size = 20
+  }
   tags = {
-    Name = "jenkins"
+    Name = "dufloth-jenkins"
   }
   vpc_security_group_ids = ["${aws_security_group.jenkins.id}"]
 }
@@ -19,6 +49,7 @@ resource "aws_instance" "jenkins" {
 resource "aws_security_group" "jenkins" {
   name        = "acessos_jenkins"
   description = "acessos_jenkins inbound traffic"
+  vpc_id      = "vpc-0404e2502328d5e45"
 
   ingress = [
     {
@@ -26,7 +57,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 22
       to_port          = 22
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -37,7 +68,7 @@ resource "aws_security_group" "jenkins" {
       from_port        = 8080
       to_port          = 8080
       protocol         = "tcp"
-      cidr_blocks      = ["${chomp(data.http.myip.body)}/32"]
+      cidr_blocks      = ["0.0.0.0/0"]
       ipv6_cidr_blocks = ["::/0"]
       prefix_list_ids  = null,
       security_groups : null,
@@ -60,7 +91,7 @@ resource "aws_security_group" "jenkins" {
   ]
 
   tags = {
-    Name = "jenkins-lab"
+    Name = "dufloth-jenkins-lab"
   }
 }
 
