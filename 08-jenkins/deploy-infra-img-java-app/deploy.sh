8c41726ee0dee1214fca7b2875e06404692ec4e5
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 15:12:11 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,11 +1,19 @@
+#!/bin/bash
+
 cd /var/lib/jenkins/workspace/Deploy/08-jenkins/deploy-infra-img-java-app/terraform 
 /usr/local/bin/terraform init 
-/usr/local/bin/terraform apply -auto-approve 
-echo "Aguardando criação de maquinas ..." 
-sleep 10 # 10 segundos 
-echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo 
-echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ", 
-echo "Aguardando criação de maquinas ..." 
-sleep 10 # 10 segundos 
-cd ../ansible 
-ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa 
+/usr/local/bin/terraform apply -auto-approve
+
+echo "Aguardando criação de maquinas ..."
+# sleep 10 # 10 segundos
+
+echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo
+echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+
+echo "Aguardando criação de maquinas ..."
+# sleep 10 # 10 segundos
+
+cd ../ansible
+
+echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa ]"
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa 
