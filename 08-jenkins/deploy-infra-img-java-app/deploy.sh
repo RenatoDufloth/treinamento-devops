40fc2935ade5e75de3f824e0b0d6bc8dbe98e65a
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 14:36:06 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,4 +1,4 @@
-cd 08-jenkins/deploy-infra-img-java-app/terraform
+cd /var/lib/jenkins/workspace/Deploy/08-jenkins/deploy-infra-img-java-app/terraform
 /usr/local/bin/terraform init 
 /usr/local/bin/terraform apply -auto-approve
 
