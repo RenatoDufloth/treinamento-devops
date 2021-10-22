22c25d9164a49b58ca87dd1d396ee7e94690b8b8
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 14:06:42 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,6 +1,6 @@
 cd ~/treinamento-devops/08-jenkins/deploy-infra-img-java-app/terraform
-~/usr/local/bin/terraform/terraform init
-~/usr/local/bin/terraform/terraform apply -auto-approve
+/usr/local/bin/terraform/terraform init
+/usr/local/bin/terraform/terraform apply -auto-approve
 
 echo "Aguardando criação de maquinas ..."
 sleep 10 # 10 segundos
