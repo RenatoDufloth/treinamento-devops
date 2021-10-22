407b2668ab512d94e5aae4c4c9a2edf94a05cf37
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 16:03:23 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -6,12 +6,12 @@ echo "Aguardando criação de maquinas ..."
 # sleep 10 # 10 segundos 
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo 
-echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ", 
+echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> /var/lib/jenkins/workspace/InfraPipeline/08-jenkins/deploy-infra-img-java-app/ansible/hosts # captura output faz split de espaco e replace de ", 
 
 echo "Aguardando criação de maquinas ..." 
 # sleep 10 # 10 segundos 
 
-cd ../ansible 
+cd /var/lib/jenkins/workspace/InfraPipeline/08-jenkins/deploy-infra-img-java-app/ansible
 
 echo "Executando ansible ::::: [ ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa ]" 
 ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa 
