1e897b3d90a2d9ab7b9a8ecef850065748963d09
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 15:03:40 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -5,7 +5,7 @@ cd /var/lib/jenkins/workspace/Deploy/08-jenkins/deploy-infra-img-java-app/terraf
 echo "Aguardando criação de maquinas ..." 
 sleep 10 # 10 segundos 
 
-echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo 
+echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo  
 echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ", 
 
 echo "Aguardando criação de maquinas ..." 
