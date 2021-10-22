dc434cf4550f419d8b7d535fadd1284403adf1e7
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 17:30:08 -0300

    Update test.sh

--- 08-jenkins/deploy-infra-img-java-app/test.sh
+++ 08-jenkins/deploy-infra-img-java-app/test.sh
@@ -3,12 +3,7 @@ cd /var/lib/jenkins/workspace/InfraPipeline/08-jenkins/deploy-infra-img-java-app
 #uri=$(/usr/local/bin/terraform output | grep public_ip | awk '{print $2;exit}' | sed -e "s/\",//g")
 uri=$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}' | sed -e "s/\",//g")
 
-
-cat uri
-cat $(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}' | sed -e "s/\",//g")
 echo $uri
-
-
 
 body=$(curl "http://$uri")
 
