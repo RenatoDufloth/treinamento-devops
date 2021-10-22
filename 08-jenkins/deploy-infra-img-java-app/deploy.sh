325d82f6d05f0703c430e15c897efb6de3f4ef72
RenatoDufloth <74377649+RenatoDufloth@users.noreply.github.com>
2021-10-22 14:59:42 -0300

    Update deploy.sh

--- 08-jenkins/deploy-infra-img-java-app/deploy.sh
+++ 08-jenkins/deploy-infra-img-java-app/deploy.sh
@@ -1,17 +1,17 @@
 
-cd /var/lib/jenkins/workspace/Deploy/08-jenkins/deploy-infra-img-java-app/terraform
-/usr/local/bin/terraform init
-/usr/local/bin/terraform apply -auto-approve
+cd /var/lib/jenkins/workspace/Deploy/08-jenkins/deploy-infra-img-java-app/terraform 
+/usr/local/bin/terraform init 
+/usr/local/bin/terraform apply -auto-approve 
 
 echo "Aguardando criação de maquinas ..." 
 sleep 10 # 10 segundos 
 
 echo $"[ec2-dev-img-jenkins]" > ../ansible/hosts # cria arquivo 
-echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ",
+echo "$(/usr/local/bin/terraform output | grep public_dns | awk '{print $2;exit}')" | sed -e "s/\",//g" >> ../ansible/hosts # captura output faz split de espaco e replace de ", 
 
-echo "Aguardando criação de maquinas ..."
+echo "Aguardando criação de maquinas ..." 
 sleep 10 # 10 segundos 
 
-cd ../ansible
+cd ../ansible 
 
-ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa
+ansible-playbook -i hosts provisionar.yml -u ubuntu --private-key ssh -i ~/.ssh/id_rsa 
